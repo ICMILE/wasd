f7a8abf6e7c8487eeaca9dc734b8d66463f3e058
ICMILE <135722510+ICMILE@users.noreply.github.com>
2025-04-19 11:23:41 +0800

    Create Adaptive configuration system.js

---
+++ Adaptive configuration system.js
@@ -0,0 +1,10 @@
+{
+    "color_profiles": {
+      "red": {"lower": [0,120,70], "upper": [10,255,255]},
+      "blue": {"lower": [110,50,50], "upper": [130,255,255]}
+    },
+    "swipe_params": {
+      "base_speed": 0.35,
+      "acceleration": 1.2
+    }
+  }
